86423dfba0fca2d57d23fd83ecdf121fd6a8cf97
Ashish Rao <ashishrao2598@gmail.com>
2018-03-29 20:07:08 +0530

    Added funtionality to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
