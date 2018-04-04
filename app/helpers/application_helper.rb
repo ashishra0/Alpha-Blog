c66b869489e1a5eef7ca50d11e04640dfe74f2f5
Ashish Rao <ashishrao2598@gmail.com>
2018-04-04 18:47:06 +0530

    Refactoring and bug fixing in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     end
 
     def popular_category
-        @categories = Category.order(@article.present?).limit(5)
+        @categories = Category.all.limit(5)
     end
 
 end
